c21dfab744efd4524ff142a2a0604671ff83a3fa
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2021-10-16 03:26:48 -0400

    Migrate crkbd over

---
+++ keyboards/crkbd/keymaps/thisishuey/README.md
@@ -0,0 +1,11 @@
+# Huey's layout for Corne
+
+Based off Soundmonster's layout
+
+Features:
+
+-   Modern OLED support (many thanks to @drashna):
+    -   Proper orientation
+    -   Graphic layer indicator ↑ ↓
+    -   Graphic modifier indicator ⌘ ⇧ ⌥ ⌃
+-   Full per-key RGB Matrix support out of the box (you still have to go through the nightmare of soldering yourself though 😉)
---
+++ keyboards/crkbd/keymaps/thisishuey/keymap.c
@@ -0,0 +1,349 @@
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+#ifdef OLED_DRIVER_ENABLE
+  static uint32_t oled_timer = 0;
+#endif
+
+extern uint8_t is_master;
+
+enum layers {
+  _QWERTY,
+  _MOUSE,
+  _NUMBER,
+  _NAVIGATION,
+  _SHORTCUT,
+  _FUNCTION,
+  _MEDIA
+};
+
+#define SFT_A LSFT_T(KC_A)
+#define CTL_S LCTL_T(KC_S)
+#define ALT_D LALT_T(KC_D)
+#define GUI_F LGUI_T(KC_F)
+#define GUI_J RGUI_T(KC_J)
+#define ALT_K RALT_T(KC_K)
+#define CTL_L RCTL_T(KC_L)
+#define SFT_SCN RSFT_T(KC_SCLN)
+
+#define PRV_SCN LSFT(KC_LEFT)
+#define NXT_SCN RSFT(KC_RGHT)
+#define OSM_MEH OSM(KC_MEH)
+#define OSM_HPR OSM(KC_HYPR)
+
+#define NAV_1 LT(_MOUSE, KC_ESC)
+#define NAV_2 LT(_NUMBER, KC_BSPC)
+#define NAV_3 LT(_NAVIGATION, KC_DEL)
+#define NAV_4 LT(_SHORTCUT, KC_TAB)
+#define NAV_5 LT(_FUNCTION, KC_SPC)
+#define NAV_6 LT(_MEDIA, KC_ENT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        PRV_SCN,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, NXT_SCN,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        OSM_MEH,   SFT_A,   CTL_S,   ALT_D,   GUI_F,    KC_G,                        KC_H,   GUI_J,   ALT_K,   CTL_L, SFT_SCN, OSM_MEH,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        OSM_HPR,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, OSM_HPRf,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              NAV_1,   NAV_2,   NAV_3,     NAV_6,   NAV_5,   NAV_4
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_MOUSE] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          RESET,   KC_NO, KC_ACL2, KC_ACL1, KC_ACL0,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_NO,                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,   KC_NO,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,   KC_NO,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_BTN2, KC_BTN3
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_NUMBER] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_EQL,    KC_7,    KC_8,    KC_9,    KC_0,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_NO,                     KC_MINS,    KC_4,    KC_5,    KC_6, KC_QUOT,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              KC_NO,   NAV_1,   KC_NO,   KC_LBRC, KC_RBRC,   KC_NO
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_NAVIGATION] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO, KC_PSCR, KC_SLCK, KC_PAUS,   KC_NO,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_NO,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_INS,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_DEL,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_SHORTCUT] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                             KC_CUT, KC_COPY, KC_PSTE,     KC_NO,   KC_NO,   KC_NO
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_FUNCTION] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NO,                       KC_NO, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO
+                                       // |--------|--------|--------| |--------|--------|--------|
+  ),
+
+  [_MEDIA] = LAYOUT(
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO, KC_FIND, KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT,                       KC_NO, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|                   |--------|--------|--------|--------|--------|--------|
+          KC_NO,   KC_NO, KC_MRWD, KC_EJCT, KC_MUTE, KC_MFFD,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,
+    // |-------|--------|--------|--------|--------|--------|--------| |--------|--------|--------|--------|--------|--------|--------|
+                                              KC_NO, KC_MPLY, KC_MSTP,     KC_NO,   KC_NO,   KC_NO
+                                       // |--------|--------|--------| |--------|--------|--------|
+  )
+};
+
+#ifdef OLED_DRIVER_ENABLE
+  oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;
+  }
+
+  void render_space(void) {
+    oled_write_P(PSTR("     "), false);
+  }
+
+  void render_mod_status_gui_alt(uint8_t modifiers) {
+    static const char PROGMEM gui_off_1[] = {0x85, 0x86, 0};
+    static const char PROGMEM gui_off_2[] = {0xa5, 0xa6, 0};
+    static const char PROGMEM gui_on_1[] = {0x8d, 0x8e, 0};
+    static const char PROGMEM gui_on_2[] = {0xad, 0xae, 0};
+
+    static const char PROGMEM alt_off_1[] = {0x87, 0x88, 0};
+    static const char PROGMEM alt_off_2[] = {0xa7, 0xa8, 0};
+    static const char PROGMEM alt_on_1[] = {0x8f, 0x90, 0};
+    static const char PROGMEM alt_on_2[] = {0xaf, 0xb0, 0};
+
+    // fillers between the modifier icons bleed into the icon frames
+    static const char PROGMEM off_off_1[] = {0xc5, 0};
+    static const char PROGMEM off_off_2[] = {0xc6, 0};
+    static const char PROGMEM on_off_1[] = {0xc7, 0};
+    static const char PROGMEM on_off_2[] = {0xc8, 0};
+    static const char PROGMEM off_on_1[] = {0xc9, 0};
+    static const char PROGMEM off_on_2[] = {0xca, 0};
+    static const char PROGMEM on_on_1[] = {0xcb, 0};
+    static const char PROGMEM on_on_2[] = {0xcc, 0};
+
+    if(modifiers & MOD_MASK_GUI) {
+      oled_write_P(gui_on_1, false);
+    } else {
+      oled_write_P(gui_off_1, false);
+    }
+
+    if ((modifiers & MOD_MASK_GUI) && (modifiers & MOD_MASK_ALT)) {
+      oled_write_P(on_on_1, false);
+    } else if(modifiers & MOD_MASK_GUI) {
+      oled_write_P(on_off_1, false);
+    } else if(modifiers & MOD_MASK_ALT) {
+      oled_write_P(off_on_1, false);
+    } else {
+      oled_write_P(off_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_ALT) {
+      oled_write_P(alt_on_1, false);
+    } else {
+      oled_write_P(alt_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_GUI) {
+      oled_write_P(gui_on_2, false);
+    } else {
+      oled_write_P(gui_off_2, false);
+    }
+
+    if (modifiers & MOD_MASK_GUI & MOD_MASK_ALT) {
+      oled_write_P(on_on_2, false);
+    } else if(modifiers & MOD_MASK_GUI) {
+      oled_write_P(on_off_2, false);
+    } else if(modifiers & MOD_MASK_ALT) {
+      oled_write_P(off_on_2, false);
+    } else {
+      oled_write_P(off_off_2, false);
+    }
+
+    if(modifiers & MOD_MASK_ALT) {
+      oled_write_P(alt_on_2, false);
+    } else {
+      oled_write_P(alt_off_2, false);
+    }
+  }
+
+  void render_mod_status_ctrl_shift(uint8_t modifiers) {
+    static const char PROGMEM ctrl_off_1[] = {0x89, 0x8a, 0};
+    static const char PROGMEM ctrl_off_2[] = {0xa9, 0xaa, 0};
+    static const char PROGMEM ctrl_on_1[] = {0x91, 0x92, 0};
+    static const char PROGMEM ctrl_on_2[] = {0xb1, 0xb2, 0};
+
+    static const char PROGMEM shift_off_1[] = {0x8b, 0x8c, 0};
+    static const char PROGMEM shift_off_2[] = {0xab, 0xac, 0};
+    static const char PROGMEM shift_on_1[] = {0xcd, 0xce, 0};
+    static const char PROGMEM shift_on_2[] = {0xcf, 0xd0, 0};
+
+    // fillers between the modifier icons bleed into the icon frames
+    static const char PROGMEM off_off_1[] = {0xc5, 0};
+    static const char PROGMEM off_off_2[] = {0xc6, 0};
+    static const char PROGMEM on_off_1[] = {0xc7, 0};
+    static const char PROGMEM on_off_2[] = {0xc8, 0};
+    static const char PROGMEM off_on_1[] = {0xc9, 0};
+    static const char PROGMEM off_on_2[] = {0xca, 0};
+    static const char PROGMEM on_on_1[] = {0xcb, 0};
+    static const char PROGMEM on_on_2[] = {0xcc, 0};
+
+    if(modifiers & MOD_MASK_CTRL) {
+      oled_write_P(ctrl_on_1, false);
+    } else {
+      oled_write_P(ctrl_off_1, false);
+    }
+
+    if ((modifiers & MOD_MASK_CTRL) && (modifiers & MOD_MASK_SHIFT)) {
+      oled_write_P(on_on_1, false);
+    } else if(modifiers & MOD_MASK_CTRL) {
+      oled_write_P(on_off_1, false);
+    } else if(modifiers & MOD_MASK_SHIFT) {
+      oled_write_P(off_on_1, false);
+    } else {
+      oled_write_P(off_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_SHIFT) {
+      oled_write_P(shift_on_1, false);
+    } else {
+      oled_write_P(shift_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_CTRL) {
+      oled_write_P(ctrl_on_2, false);
+    } else {
+      oled_write_P(ctrl_off_2, false);
+    }
+
+    if (modifiers & MOD_MASK_CTRL & MOD_MASK_SHIFT) {
+      oled_write_P(on_on_2, false);
+    } else if(modifiers & MOD_MASK_CTRL) {
+      oled_write_P(on_off_2, false);
+    } else if(modifiers & MOD_MASK_SHIFT) {
+      oled_write_P(off_on_2, false);
+    } else {
+      oled_write_P(off_off_2, false);
+    }
+
+    if(modifiers & MOD_MASK_SHIFT) {
+      oled_write_P(shift_on_2, false);
+    } else {
+      oled_write_P(shift_off_2, false);
+    }
+  }
+
+  void render_logo(void) {
+    static const char PROGMEM corne_logo[] = {
+      0x80, 0x81, 0x82, 0x83, 0x84,
+      0xa0, 0xa1, 0xa2, 0xa3, 0xa4,
+      0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0};
+    oled_write_P(corne_logo, false);
+    oled_write_P(PSTR("corne"), false);
+  }
+
+  void render_layer_state(uint8_t base) {
+    static const char PROGMEM default_layer[] = {
+      0x20, 0x9d, 0x9e, 0x9f, 0x20,
+      0x20, 0xbd, 0xbe, 0xbf, 0x20,
+      0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
+    static const char PROGMEM low_layer[] = {
+      0x20, 0x9a, 0x9b, 0x9c, 0x20,
+      0x20, 0xba, 0xbb, 0xbc, 0x20,
+      0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
+    static const char PROGMEM mid_layer[] = {
+      0x20, 0x94, 0x95, 0x96, 0x20,
+      0x20, 0xb4, 0xb5, 0xb6, 0x20,
+      0x20, 0xd4, 0xd5, 0xd6, 0x20, 0};
+    static const char PROGMEM high_layer[] = {
+      0x20, 0x97, 0x98, 0x99, 0x20,
+      0x20, 0xb7, 0xb8, 0xb9, 0x20,
+      0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
+    if(layer_state_is(base)) {
+      oled_write_P(low_layer, false);
+    } else if(layer_state_is(base + 1)) {
+      oled_write_P(mid_layer, false);
+    } else if(layer_state_is(base + 2)) {
+      oled_write_P(high_layer, false);
+    } else {
+      oled_write_P(default_layer, false);
+    }
+  }
+
+  void render_status_main(void) {
+    render_logo();
+    render_space();
+    render_layer_state(4);
+    render_space();
+    render_mod_status_gui_alt(get_mods()|get_oneshot_mods());
+    render_mod_status_ctrl_shift(get_mods()|get_oneshot_mods());
+  }
+
+  void render_status_secondary(void) {
+    render_logo();
+    render_space();
+    render_layer_state(1);
+    render_space();
+    render_mod_status_gui_alt(get_mods()|get_oneshot_mods());
+    render_mod_status_ctrl_shift(get_mods()|get_oneshot_mods());
+  }
+
+  void oled_task_user(void) {
+    if (timer_elapsed32(oled_timer) > 30000) {
+      oled_off();
+      return;
+    }
+    #ifndef SPLIT_KEYBOARD
+      else { oled_on(); }
+    #endif
+
+    if (is_master) {
+      render_status_main();
+    } else {
+      render_status_secondary();
+    }
+  }
+
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    #ifdef OLED_DRIVER_ENABLE
+      oled_timer = timer_read32();
+    #endif
+  }
+  return true;
+}
